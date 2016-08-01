047a6bf7f22016501bd4b9299907d791fb225910
Brian Byers <bbyers@pivotal.io>
2016-08-01 14:40:35 -0700

    git update

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+exec 3>&1 # make stdout available as fd 3 for the result
+exec 1>&2 # redirect all output to stderr for logging
+
 load_pubkey() {
   mkdir ~/.ssh
   # touch ~/.ssh/known_hosts
