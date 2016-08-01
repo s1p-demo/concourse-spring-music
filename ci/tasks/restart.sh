aa804ffd73d9d2820fe400b955e1811d25381a80
Brian Byers <bbyers@pivotal.io>
2016-08-01 16:33:18 -0700

    git update

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -3,17 +3,17 @@ set -e
 
 git clone https://$password@github.com/s1p-demo/concourse-spring-music.git
 
+color=`cat blue-green-file/color-1.0.0`
+echo $color
+
+# make it colorful!
 cd concourse-spring-music
 cd bin
 
 git config --global user.email "bbyers@pivotal.io"
 git config --global user.name "Concourse"
 git config --global push.default matching
-
-color=`cat blue-green-file/color-1.0.0`
-echo $color
 # ./makeItBlue.sh
-
 cd ../../
 echo "green" > blue-green-output/color-1.0.0
 ls blue-green-output
