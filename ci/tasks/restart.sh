e267f17ace18c43b57bf0aebf547acb98f0287c5
Brian Byers <bbyers@pivotal.io>
2016-08-01 12:15:45 -0700

    git config

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -5,6 +5,10 @@ set -e
 # ls
 cd git-repo
 cd bin
+git config --global user.email "bbyers@pivotal.io"
+git config --global user.name "Concourse"
+git config --global push.default matching
+
 ./makeItGreen.sh
 # ls blue-green
 # ls blue-green-output
