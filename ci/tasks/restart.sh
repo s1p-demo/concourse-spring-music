a8c7ec74ca04fed4fb91bc39bd5eca9de30d99be
Brian Byers <bbyers@pivotal.io>
2016-08-01 14:09:37 -0700

    git update

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -9,7 +9,7 @@ echo $SSH_KEY > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 eval `ssh-agent -s`
 ls ~/.ssh
-openssl rsa -in ~/.ssh/id_rsa -out ~/.ssh/id_rsa
+# openssl rsa -in ~/.ssh/id_rsa -out ~/.ssh/id_rsa
 ssh-add ~/.ssh/id_rsa
 git clone $GIT_REPO
 # ls
