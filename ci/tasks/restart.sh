c233f8be2ad6ec13732088121e47992c6a31405f
Brian Byers <bbyers@pivotal.io>
2016-08-01 16:41:24 -0700

    git update

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -4,7 +4,6 @@ set -e
 git clone https://$password@github.com/s1p-demo/concourse-spring-music.git
 
 color=`cat blue-green-file/color-1.0.0`
-echo $color
 
 # make it colorful!
 cd concourse-spring-music
@@ -16,9 +15,11 @@ git config --global push.default matching
 
 echo $color
 if [ "$color" == "blue" ]; then
+  echo "turning banner green"
   ./makeItGreen.sh
   echo "green" > ../../blue-green-output/color-1.0.0
 else
+  echo "turning banner blue"
   ./makeItBlue.sh
   echo "blue" > ../../blue-green-output/color-1.0.0
 fi
