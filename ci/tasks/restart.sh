c617d3fde09c71e310d75fbdbeb687f690f7c6bd
Brian Byers <bbyers@pivotal.io>
2016-08-01 13:57:50 -0700

    git config

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 set -e
 
-# mkdir ~/.ssh
-# touch ~/.ssh/known_hosts
-# ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+mkdir ~/.ssh
+touch ~/.ssh/known_hosts
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 # echo "-----BEGIN RSA PRIVATE KEY-----
 #
 #   -----END RSA PRIVATE KEY-----" > ~/.ssh/id_rsa
-# chmod 600 ~/.ssh/id_rsa
-# git clone $GIT_REPO
+echo $SSH_KEY > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+git clone $GIT_REPO
 # ls
-# cd concourse-spring-music
-#cd bin
+cd concourse-spring-music
+cd bin
 # ls blue-green
 # ls blue-green-output
-# git config --global user.email "bbyers@pivotal.io"
-# git config --global user.name "Concourse"
-# git config --global push.default matching
+git config --global user.email "bbyers@pivotal.io"
+git config --global user.name "Concourse"
+git config --global push.default matching
 
-# cd git-repo/concourse-spring-music/bin
+cd concourse-spring-music/bin
 # ./makeItBlue.sh
-cp git-repo/src/main/webapp/assets/css/blue-app.css git-repo/src/main/webapp/assets/css/app.css
-# git add git-repo/src/main/webapp/assets/css/app.css
+cp src/main/webapp/assets/css/blue-app.css src/main/webapp/assets/css/app.css
+git add src/main/webapp/assets/css/app.css
