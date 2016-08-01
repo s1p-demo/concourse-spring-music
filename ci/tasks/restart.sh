49f6e1667667c9b672ce1e78cada77171e2b6742
Brian Byers <bbyers@pivotal.io>
2016-08-01 13:51:01 -0700

    git config

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -5,20 +5,20 @@ set -e
 # touch ~/.ssh/known_hosts
 # ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 # echo "-----BEGIN RSA PRIVATE KEY-----
-#  
+#
 #   -----END RSA PRIVATE KEY-----" > ~/.ssh/id_rsa
 # chmod 600 ~/.ssh/id_rsa
 # git clone $GIT_REPO
 # ls
 # cd concourse-spring-music
 #cd bin
-ls blue-green
+# ls blue-green
 # ls blue-green-output
 # git config --global user.email "bbyers@pivotal.io"
 # git config --global user.name "Concourse"
 # git config --global push.default matching
 
-cd concourse-spring-music/bin
+cd git-repo/concourse-spring-music/bin
 # ./makeItBlue.sh
 cp ../src/main/webapp/assets/css/blue-app.css ../src/main/webapp/assets/css/app.css
 git add ../src/main/webapp/assets/css/app.css
