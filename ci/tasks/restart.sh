46292e314b697f677812909db7a4ca272bc707ed
Brian Byers <bbyers@pivotal.io>
2016-08-01 16:37:03 -0700

    git update

--- ci/tasks/restart.sh
+++ ci/tasks/restart.sh
@@ -14,7 +14,7 @@ git config --global user.email "bbyers@pivotal.io"
 git config --global user.name "Concourse"
 git config --global push.default matching
 
-if [[ "$color" == "blue" ]]
+if [ "$color" == "blue" ]; then
   ./makeItGreen.sh
   echo "green" > ../../blue-green-output/color-1.0.0
 else
